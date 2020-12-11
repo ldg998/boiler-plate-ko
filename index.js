b75a470ee48610eb35a0fddfb39e01278ec036b6
donggunDi <ldg998@tobesystem.co.kr>
2020-12-11 16:31:09 +0900

    테스트

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
   // 회원가입 할때 필요한 정보들을 클라이언트에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
-  
+
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ sucess: false, err })
@@ -95,7 +95,6 @@ app.get('/api/users/logout', auth, (req, res) => {
     { token: "" },
      (err, user) => {
     if (err) return res.json({ success: false, err });
-
     return res.status(200), send({
       success: true
     })
